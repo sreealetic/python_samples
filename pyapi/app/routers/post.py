637ca5beafa7b29498c999293a85e3b6d228cadf
Sree Harsha Aleti <sreeharsha_a@yahoo.com>
2022-09-06 19:43:33 -0400

    update

--- pyapi/app/routers/post.py
+++ pyapi/app/routers/post.py
@@ -25,6 +25,20 @@ def get_posts(id: int , db: Session = Depends(get_db)):
     return  post
 
 
+@router.put("/posts/{id}", response_model= schemas.Post)
+def update_posts(id: int , up_post : schemas.PostCreate, db: Session = Depends(get_db)):
+    post_query = db.query(dbmodels.Post).filter(dbmodels.Post.id == id)
+    post  = post_query.first()
+
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id: {id} doesnt not exist")
+    
+    post_query.update(up_post.dict(), synchronize_session=False)
+
+    db.commit()
+
+    return  post_query.first()
+    
 @router.post("/posts", response_model=schemas.Post)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
